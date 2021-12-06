a88d75ed4bb1504446b2be8509d1f33f3fe3fce1
indritalls <95203855+indritalls@users.noreply.github.com>
2021-12-06 09:05:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,8 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg_from_user = event.message.text
+    if msg_from_user == 'Data-covid':
     message = TextSendMessage(text='Hello, world')
     line_bot_api.reply_message(event.reply_token, message)
 
